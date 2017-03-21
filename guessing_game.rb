cd35bf6089337e6c30d189559fc092b157008d29
Sherilyn Moline <smoline@gmail.com>
2017-03-21 17:22:06 -0400

    Epic Mode Final

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,45 +1,48 @@
 class Game
   def initialize
     @random_num = rand(101) + 1
+    @guess_array = []
     # @random_num = 25
   end
 
   def ask_for_number
     print "Guess a number between 1 and 100: "
     answer = gets.chomp.to_i
-    return answer
+    answer
   end
 
-  def duplicate_number_test(g_array)
-    i = 0
-    while i < (g_array.length - 1)
-      if g_array.last == g_array[i]
-        puts "You already used that number, are you even trying?"
-        break
-      else
-        i += 1
-      end
+  def duplicate_number_test(guess)
+    if @guess_array.include?(guess)
+      puts "You already used that number, are you even trying?"
+    end
+  end
+
+  def silly_guess(guess)
+    if @guess_array.last < @random_num && guess < @guess_array.last
+      puts "Now you are just wasting your guesses"
+    elsif @guess_array.last > @random_num && guess > @guess_array.last
+      puts "Now you are just wasting your guesses"
     end
   end
 
   def play
-
     guess_count = 1
     guess = ask_for_number
-    guess_array = []
-    guess_array << guess
+    @guess_array << guess
 
     while guess_count < 5
       if guess < @random_num
         puts "Your guess is lower than my number."
         guess = ask_for_number
-        guess_array << guess
-        duplicate_number_test(guess_array)
+        duplicate_number_test(guess)
+        silly_guess(guess)
+        @guess_array << guess
       elsif guess > @random_num
         puts "Your guess is higher than my number"
         guess = ask_for_number
-        guess_array << guess
-        duplicate_number_test(guess_array)
+        duplicate_number_test(guess)
+        silly_guess(guess)
+        @guess_array << guess
       else
         break
       end
