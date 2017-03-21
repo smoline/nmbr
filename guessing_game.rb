58ed5841ee9e517a41d51f63932a36cf6def8bfe
Sherilyn Moline <smoline@gmail.com>
2017-03-21 15:51:12 -0400

    Epic Mode Mostly

--- guessing_game.rb
+++ guessing_game.rb
@@ -1,50 +1,56 @@
-def ask_for_number
-  print "Guess a number between 1 and 100: "
-  answer = gets.chomp.to_i
-  return answer
-end
+class Game
+  def initialize
+    @random_num = rand(101) + 1
+    # @random_num = 25
+  end
 
-def duplicate_number_test(g_array)
-  i = 0
-  while i < (g_array.length - 1)
-    if g_array.last == g_array[i]
-      puts "You already used that number, are you even trying?"
-      break
+  def ask_for_number
+    print "Guess a number between 1 and 100: "
+    answer = gets.chomp.to_i
+    return answer
+  end
+
+  def duplicate_number_test(g_array)
+    i = 0
+    while i < (g_array.length - 1)
+      if g_array.last == g_array[i]
+        puts "You already used that number, are you even trying?"
+        break
+      else
+        i += 1
+      end
+    end
+  end
+
+  def play
+
+    guess_count = 1
+    guess = ask_for_number
+    guess_array = []
+    guess_array << guess
+
+    while guess_count < 5
+      if guess < @random_num
+        puts "Your guess is lower than my number."
+        guess = ask_for_number
+        guess_array << guess
+        duplicate_number_test(guess_array)
+      elsif guess > @random_num
+        puts "Your guess is higher than my number"
+        guess = ask_for_number
+        guess_array << guess
+        duplicate_number_test(guess_array)
+      else
+        break
+      end
+      guess_count += 1
+    end
+    if guess == @random_num
+      puts "You win!"
     else
-      i += 1
+      puts "You lose!"
     end
   end
 end
 
-def guessing
-  guess_count = 1
-  guess = ask_for_number
-  random_num = rand(101) + 1
-  # random_num = 25
-  guess_array = []
-  guess_array << guess
-
-  while guess_count < 5
-    if guess < random_num
-      puts "Your guess is lower than my number."
-      guess = ask_for_number
-      guess_array << guess
-      duplicate_number_test(guess_array)
-    elsif guess > random_num
-      puts "Your guess is higher than my number"
-      guess = ask_for_number
-      guess_array << guess
-      duplicate_number_test(guess_array)
-    else
-      break
-    end
-    guess_count += 1
-  end
-  if guess == random_num
-    puts "You win!"
-  else
-    puts "You lose!"
-  end
-end
-
-guessing
+Game.new.play
